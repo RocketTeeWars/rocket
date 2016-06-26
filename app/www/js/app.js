67dec46010381ad997f662604828e2303b68375a
RocketTeeWars <rocket@teewars.org>
2016-06-26 17:19:41 +0200

    Removed ngTouchmove directive

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -54,31 +54,6 @@ angular.module("app", [])
     }
 })
 
-.directive("ngTouchmove", function () {
-    return {
-        controller: ["$scope", "$element", function ($scope, $element) {
-
-            $element.bind("touchstart", onTouchStart);
-            function onTouchStart(event) {
-                event.preventDefault();
-                $element.bind("touchmove", onTouchMove);
-                $element.bind("touchend", onTouchEnd);
-            }
-            function onTouchMove(event) {
-                var method = $element.attr("ng-touchmove");
-                $scope.$event = event;
-                $scope.$apply(method);
-            }
-            function onTouchEnd(event) {
-                event.preventDefault();
-                $element.unbind("touchmove", onTouchMove);
-                $element.unbind("touchend", onTouchEnd);
-            }
-
-        }]
-    }
-})
-
 .directive("ngTouchend", function () {
     return {
         controller: ["$scope", "$element", function ($scope, $element) {
